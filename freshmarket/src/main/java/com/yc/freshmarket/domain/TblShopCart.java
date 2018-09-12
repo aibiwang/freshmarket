4b969c6de49d684dd8b4323e086c0ed4ca54b36c
1171797207 <1171797207@qq.com>
2018-09-12 12:04:44 +0800

    修改联合主键

--- freshmarket/src/main/java/com/yc/freshmarket/domain/TblShopCart.java
+++ freshmarket/src/main/java/com/yc/freshmarket/domain/TblShopCart.java
@@ -1,45 +1,54 @@
 package com.yc.freshmarket.domain;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity	//实体类注解
 @Table(name="tbl_shopcart")	// 配置表名
 public class TblShopCart {
+	
 	@Id	//注解注解
+	@GeneratedValue(strategy=GenerationType.AUTO)	//配置主键值的生成机制
+	private Integer cartitemId;
 	private Integer	cartId;
-	@Id	//注解注解
+
 	private Integer	goodsId;
 	private double	goodscount;
-	
-	@Override
-	public String toString() {
-		return "TblShopCart [cartId=" + cartId + ", goodsId=" + goodsId + ", goodscount=" + goodscount + "]";
+	public Integer getCartitemId() {
+		return cartitemId;
 	}
-
+	public void setCartitemId(Integer cartitemId) {
+		this.cartitemId = cartitemId;
+	}
 	public Integer getCartId() {
 		return cartId;
 	}
-
 	public void setCartId(Integer cartId) {
 		this.cartId = cartId;
 	}
-
 	public Integer getGoodsId() {
 		return goodsId;
 	}
-
 	public void setGoodsId(Integer goodsId) {
 		this.goodsId = goodsId;
 	}
-
 	public double getGoodscount() {
 		return goodscount;
 	}
-
 	public void setGoodscount(double goodscount) {
 		this.goodscount = goodscount;
 	}
+	@Override
+	public String toString() {
+		return "TblShopCart [cartitemId=" + cartitemId + ", cartId=" + cartId
+				+ ", goodsId=" + goodsId + ", goodscount=" + goodscount + "]";
+	}
 	
+	
+	
+
 }
